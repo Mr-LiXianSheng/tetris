fb71192f2210164272e33589aa5dbc0f6f3dc068
jiaming <743192023@qq.com>
2018-08-24 09:20:45 +0800

    do some BE router

--- BE_API/route/index.php
+++ BE_API/route/index.php
@@ -8,7 +8,21 @@ $requestPath = explode('/',$_SERVER['PATH_INFO']);
 // Splice useless params
 array_splice($requestPath, 0, 1);
 
-print_r($requestPath);
+@$module = $requestPath[0];
+
+@$function = $requestPath[1];
+
+@$params = array_slice($requestPath, 2);
+
+
+
+session_start();
+
+
+
+switch ($module) {
+
+}
 
 
 ?>
