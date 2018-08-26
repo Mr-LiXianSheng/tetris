0231e35c4629de648a70e33983beebc173f2ca52
jiaming <743192023@qq.com>
2018-08-26 20:17:02 +0800

    do some update

---
+++ src/components/table/tableColumn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="table-column">
+    <solt></solt>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableColumn',
+  props: ['prop', 'label']
+}
+</script>
+
+<style lang="less">
+@import '../../assets/style/color.less';
+
+.table-column {
+  flex: 1;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  font-weight: bold;
+  color: @main-color;
+  box-shadow: 0 0 3px @main-color;
+}
+</style>
---
+++ src/components/table/tableWithSlot.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="table-with-slot">
+    <div class="table-title-container">
+      <!-- <slot></slot> -->
+    </div>
+
+    <div class="table-content-container">
+      <div
+        class="table-row"
+        v-for="(row, index) in tableData"
+        :key="row + index">
+        <slot :row="row"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableWithSlot',
+  props: ['tableData'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+    console.error(this.$children)
+  }
+}
+</script>
+
+<style lang="less">
+@import '../../assets/style/color.less';
+
+.table-with-slot {
+  width: 100%;
+  box-shadow: 0 0 3px @main-color;
+
+  .table-title-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .table-content-container {
+    .table-row {
+      display: flex;
+      height: 40px;
+      box-shadow: 0 0 3px @main-color;
+    }
+  }
+}
+</style>
