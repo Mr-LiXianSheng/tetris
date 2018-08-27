b7acb6ab100a7bfa1b19bef51b8520cd87efa174
jiaming <743192023@qq.com>
2018-08-27 19:12:51 +0800

    update pagination conponents

--- src/components/index.js
+++ src/components/index.js
@@ -6,11 +6,14 @@ import tableWithSlot from './table/tableWithSlot.vue'
 
 import tableColumn from './table/tableColumn.vue'
 
+import pagination from './pagination.vue'
+
 export default {
   install: (Vue) => {
     Vue.component('inputWithButton', inputWithButton)
     Vue.component('rowNavBar', rowNavBar)
     Vue.component('tableWithSlot', tableWithSlot)
     Vue.component('tableColumn', tableColumn)
+    Vue.component('pagination', pagination)
   }
 }
