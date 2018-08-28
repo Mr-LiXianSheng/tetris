70480a64a0282921c7fde29d93efb3c5cf4c5289
jiaming <743192023@qq.com>
2018-08-28 19:42:59 +0800

    complete register fun

--- BE_API/api/registration/register.php
+++ BE_API/api/registration/register.php
@@ -1,5 +1,41 @@
 <?php
 checkRequestMethod('post');
 
-checkRequestParams('userName', 'passWord', 'mail', 'aa');
+checkRequestParams('userName', 'passWord', 'mail');
+
+$userName = $_REQUEST['userName'];
+$passWord = $_REQUEST['passWord'];
+$mail = $_REQUEST['mail'];
+
+$result = query("SELECT `USERNAME` FROM `USER` WHERE `USERNAME` = '${userName}'");
+
+if (!$result->status) {
+  $RESPONSE->msg = 'Abnormal Data!';
+
+  throwRequestResponse();
+}
+
+if ($result->num > 0) {
+  $RESPONSE->msg = 'UserName already exists!';
+
+  throwRequestResponse();
+}
+
+$uid = uniqtag($userName);
+
+$time = time();
+
+$result = query("INSERT INTO `USER`(`UID`, `USERNAME`, `PASSWORD`, `MAIL`, `LASTLOGINTIME`, `REGTIME`)
+  VALUES ('${uid}','${userName}','${passWord}','${mail}','${time}','${time}')");
+
+if (!$result->status) {
+  $RESPONSE->msg = 'Register Fail, please try later!';
+
+  throwRequestResponse();
+}
+
+$RESPONSE->code = 'success';
+$RESPONSE->msg = 'Register Success!';
+
+throwRequestResponse();
 ?>
