e974050dd1e4428640b7378909f683a574f004d1
JM <32119686+jiaming743@users.noreply.github.com>
2018-09-05 19:25:40 +0800

    completed chat div style an send message logic fun

--- src/pages/mainContent/components/onlineChat.vue
+++ src/pages/mainContent/components/onlineChat.vue
@@ -2,30 +2,87 @@
   <div id="online-chat">
     <div class="content">
 
+      <template v-for="message in chatMessage">
+        <div class="online" v-if="message.type === 'online'" :key="message.unique">
+          <span class="info">{{ message.text }}</span>
+        </div>
+
+        <div class="offline" v-if="message.type === 'offline'" :key="message.unique">
+          <span class="info">{{ message.text }}</span>
+        </div>
+
+        <div class="other" v-if="message.type === 'other'" :key="message.unique">
+          <div class="header">
+            <span class="user-name">{{ message.USERNAME }}</span>
+            <span class="time">{{ message.time }}</span>
+          </div>
+          <div class="info">
+            <span class="text">{{ message.text }}</span>
+          </div>
+        </div>
+
+        <div class="self" v-if="message.type === 'self'" :key="message.unique">
+          <div class="header">
+            <span class="time">{{ message.time }}</span>
+            <span class="user-name">{{ message.USERNAME }}</span>
+          </div>
+          <div class="info">
+            <span class="text">{{ message.text }}</span>
+          </div>
+        </div>
+      </template>
+
     </div>
     <div class="inputer">
-      <textarea class="textarea" placeholder="Please input message"></textarea>
+      <textarea class="textarea"
+        v-model="userInput"
+        placeholder="Please input message"
+        :disabled="!websocket"
+        @keydown.enter="sendChatMessage"/>
       <div class="footer">
         Click / Enter To Send
-        <div class="send-btn">Send</div>
+        <div class="send-btn" @click="sendChatMessage">Send</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   name: 'OnlineChat',
   data () {
-    return {}
+    return {
+      // user input message
+      userInput: ''
+    }
   },
   computed: {
-    ...mapState(['chatMessage'])
+    ...mapState(['chatMessage', 'websocket', 'userBaseInfo'])
   },
   methods: {
+    sendChatMessage () {
+      const { userInput, $nextTick, userBaseInfo: { UID, USERNAME } } = this
 
+      const { sendWSMessage } = this
+
+      if (!userInput) return false
+
+      const message = {
+        type: 'chat',
+        UID,
+        USERNAME,
+        text: userInput
+      }
+
+      sendWSMessage(message)
+
+      $nextTick(e => {
+        this.userInput = ''
+      })
+    },
+    ...mapMutations(['sendWSMessage'])
   }
 }
 </script>
@@ -40,6 +97,89 @@ export default {
 
   .content {
     flex: 1;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      width: 0px;
+      height: 0px;
+    }
+
+    .online, .offline {
+      height: 40px;
+      line-height: 40px;
+      text-align: center;
+
+      .info {
+        padding: 5px 10px;
+        box-shadow: 0 0 3px @main-color;
+        background-color: @main-color;
+        color: #fff;
+        font-size: 13px;
+      }
+    }
+
+    .offline {
+      .info {
+        box-shadow: 0 0 3px @deep-color;
+        background-color: @deep-color;
+      }
+    }
+
+    .other, .self {
+      box-sizing: border-box;
+      padding: 0 10px;
+      margin-bottom: 10px;
+
+      .header {
+        height: 20px;
+        line-height: 20px;
+        font-size: 12px;
+        color: @font-thin;
+        // margin-bottom: 5px;
+
+        .user-name {
+          margin-right: 20px;
+        }
+
+        .time {
+          line-height: 22px;
+        }
+      }
+
+      .info {
+
+        .text {
+          display: inline-block;
+          word-break: break-all;
+          max-width: 80%;
+          background-color: @deep-color;
+          border-radius: 10px;
+          box-sizing: border-box;
+          padding: 5px;
+          color: #fff;
+          font-size: 14px;
+        }
+      }
+    }
+
+    .self {
+      .header {
+        text-align: right;
+
+        .user-name {
+          margin-right: 0px;
+          margin-left: 20px;
+        }
+      }
+
+      .info {
+        text-align: right;
+
+        .text {
+          text-align: left;
+          background-color: @addi-color;
+        }
+      }
+    }
   }
 
   .inputer {
